9ecfe0a2f439fc52bc3c127aba71f27f8357340d
Nazar <nazarky@ukr.net>
2016-07-17 14:52:38 +0300

    add missed files for product resource

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :price, :desctiption, :free, :created_at, :updated_at
---
+++ db/migrate/20160717102149_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :price
+      t.text :desctiption
+      t.boolean :free
+
+      t.timestamps
+    end
+  end
+end
